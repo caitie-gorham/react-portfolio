8eca0e521c3340c667c6a365030aa48bd12fca19
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-10 22:05:35 -0500

    added about component to be used on homepage

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../About/style.css";
+
+function About() {
+    return (
+        <div class="card text-center">
+            <div class="card-body">
+                <h2 class="card-title">Welcome!</h2>
+                <p>Hello! I am a developing web developer with a passion for full-stack, graphic design and creative branding.</p>
+                <a class="btn btn-outline-warning" href="https://www.canva.com/design/DAEWFMitOBc/ZKWI8ci-MNCBuBnVIs0T9Q/view?utm_content=DAEWFMitOBc&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" role="button">Download Resume</a>
+                <a class="btn btn-outline-warning" href="https://github.com/caitie-gorham" role="button">GitHub</a>
+            </div>
+        </div>
+    )
+}
+
+export default About;
