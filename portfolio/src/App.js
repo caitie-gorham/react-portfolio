b64164904ddd10010045940638b41bf225cb1df3
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-10 20:47:19 -0500

    added footer import and footer to jsx

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/index';
+import Footer from "./components/Footer/index";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
@@ -12,11 +13,12 @@ function App() {
       <Header />
       <div className="container main-container">
         <Switch>
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
         </Switch>
       </div>
+      <Footer/>
     </Router>
   )
 }
