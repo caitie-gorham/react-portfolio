2a55f440f12bc903f70e131b2b0cf21926f86b18
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-10 20:45:04 -0500

    added footer component and styling

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Footer/style.css"
+
+function Footer() {
+    return (
+        <div className="container-fluid footer-container">
+          <div className="container-md">
+            <p>Caitie Gorham   |   Proudly Made with React |   2021</p>
+          </div>
+        </div>
+    )
+}
+
+export default Footer;
