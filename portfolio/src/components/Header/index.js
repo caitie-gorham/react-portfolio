8c98bb2894b8f8e316181df337cd3055cc788448
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-10 20:45:48 -0500

    restyled Header and exported default correctly

--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -9,7 +9,7 @@ function Header() {
                     <div className="container-md">
                         <button className="navbar-brand-name" href="#">Caitie Gorham</button>
                         <div class="ml-auto navbar-nav navbar-nav-links">
-                            <a className="nav-link" href="/home">Home</a>
+                            <a className="nav-link" href="/">Home</a>
                             <a className="nav-link" href="/portfolio">Portfolio</a>
                             <a className="nav-link" href="/contact">Contact</a>
                         </div>
